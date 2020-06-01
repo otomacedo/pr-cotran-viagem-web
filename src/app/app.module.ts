41adae131d24c66c78e2bb0664339637169323e6
Oto de Macedo <otomacedo2000@gmail.com>
2020-06-01 17:53:31 -0300

    Commit inicial do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RhComponent } from './rh/rh.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RhComponent
   ],
   imports: [
     BrowserModule,
